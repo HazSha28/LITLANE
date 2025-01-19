b8049532025b8c2cbb029fa057f0fc7220ae900b
Hazeena Shahul Hameed <tohazsha@gmail.com>
2025-01-19 17:39:13 +0530

    Update java.js

--- java.js
+++ java.js
@@ -1,23 +1,20 @@
-// Simulate form submissions
 document.addEventListener("DOMContentLoaded", () => {
-    const signupForm = document.getElementById("signup-form");
-    const loginForm = document.getElementById("login-form");
-  
-    if (signupForm) {
-      signupForm.addEventListener("submit", (e) => {
-        e.preventDefault();
-        alert("Sign-Up successful! Redirecting to login...");
-        window.location.href = "login.html";
-      });
-    }
-  
-    if (loginForm) {
-      loginForm.addEventListener("submit", (e) => {
-        e.preventDefault();
-        alert("Login successful! Redirecting to home...");
-        window.location.href = "home.html";
-      });
-    }
-  });
+  const signupForm = document.getElementById("signup-form");
+  const loginForm = document.getElementById("login-form");
 
- 
+  if (signupForm) {
+    signupForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+      alert("Sign-Up successful! Redirecting to Login...");
+      window.location.href = "login.html";
+    });
+  }
+
+  if (loginForm) {
+    loginForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+      alert("Login successful! Redirecting to Home...");
+      window.location.href = "index.html";
+    });
+  }
+});
